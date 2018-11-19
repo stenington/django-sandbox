13ae0e6caa38166de082b84c84107af0de1b1e1b
Rachel Wyatt <wyattr@gmail.com>
2018-11-19 15:24:36 -0500

    list of favorites now displayed

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from favorites.views import FavoritesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', FavoritesListView.as_view(), name='favorites-list'),
+    path('favorites', FavoritesView.as_view(), name='favorites'),
 ]
